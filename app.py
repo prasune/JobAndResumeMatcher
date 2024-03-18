eef1349a743181e62d95457123df020e568f7d62
sangeethasanthosh <sangeetha90naik@gmail.com>
2024-03-18 21:55:29 +0530

    Update app.py
    Signed-off-by: sangeethasanthosh <sangeetha90naik@gmail.com>

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def search_matching_jobs():
 
 @app.route('/ping', methods=['GET'])
 def app_health_check():
-    return jsonify({'success': "200",'status':"UP})
+    return jsonify({'success': "200",'status':"UP"})
                     
 #Function to read data from google cloud storage and create a dataframe
 def get_extracted_df(input_path):
